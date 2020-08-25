ddbcdccdc87b7fb180c7da6b406c3548d344071a
KevinChen <cgh804@163.com>
2020-08-25 18:17:07 +0800

    add setState

--- main.js
+++ main.js
@@ -3,11 +3,26 @@ import { createElement, render, Component } from "./src/toy-react";
 const text = "my component";
 
 class MyComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      a: 1,
+      b: 2,
+    };
+  }
   render() {
     return (
       <div>
         <h1>{text}</h1>
-        {this.children}
+        <div>{this.state.a.toString()}</div>
+        <div>{this.state.b.toString()}</div>
+        <button
+          onClick={() => {
+            this.setState({ a: this.state.a + 1 });
+          }}
+        >
+          add
+        </button>
       </div>
     );
   }
@@ -16,7 +31,6 @@ class MyComponent extends Component {
 render(
   <MyComponent id="a" class="c">
     <div>abc</div>
-    <div>{new Date().toLocaleTimeString()}</div>
     <div></div>
   </MyComponent>,
   document.getElementById("root")
